22069e052bdabe6a4ea1ce312e9e5bd1a7e658d9
matteo.anselmi <matteo.anselmi@csttech.it>
2015-11-03 18:27:36 +0100

    add

--- src/it/csttech/test/HelloWorld.java
+++ src/it/csttech/test/HelloWorld.java
@@ -5,12 +5,12 @@ public class HelloWorld
 
     public HelloWorld()
     {
-        // TODO Auto-generated constructor stub
+        
     }
 
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
+        
 
     }
 
